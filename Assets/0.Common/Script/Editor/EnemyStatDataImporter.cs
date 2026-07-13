4627006b5d5b23a5ef110921dd56a89b171155fc
hanabi2356home <hanabira2356@gmail.com>
2026-07-14 00:45:38 +0900

    파일 위치 변경

--- Assets/0.Common/Script/Editor/EnemyStatDataImporter.cs
+++ Assets/0.Common/Script/Editor/EnemyStatDataImporter.cs
@@ -7,7 +7,7 @@ using System.Runtime.Remoting.Messaging;
 using System.Reflection;
 public class EnemyStatDataImporter : EditorWindow
 {
-	private string csvPath = "Assets/3.Data/Enemy/EnemyStatData.csv";
+	private string csvPath = "Assets/2. Enemy/Script/Data/EnemyStatData.csv";
 	[MenuItem("Tools/Stat Data Import/Enemy Stat Import")]
 	public static void ShowWindow()
 	{
