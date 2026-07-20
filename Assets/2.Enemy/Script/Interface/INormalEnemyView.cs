515643ca73fdd49b63787b1b56b811a47b2e8ff2
hanabi2356home <hanabira2356@gmail.com>
2026-07-21 02:33:07 +0900

    NormalEnemyView에 공격 실행 용 이벤트 추가

--- Assets/2.Enemy/Script/Interface/INormalEnemyView.cs
+++ Assets/2.Enemy/Script/Interface/INormalEnemyView.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 
 public interface INormalEnemyView 
 {
+	public event Action OnAttack;
     public Vector2 TargetPosition { get; }
 	public void UpdateTargetPosition(Vector2 targetPosition);
 	
--- Assets/2.Enemy/Script/View/NormalEnemyView.cs
+++ Assets/2.Enemy/Script/View/NormalEnemyView.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 
 public class NormalEnemyView : MonoBehaviour ,INormalEnemyView
 {
 	private Vector2 _targetPosition;
 	public Vector2 TargetPosition => _targetPosition;
+
+	public event Action OnAttack;
 
 	public void UpdateTargetPosition(Vector2 targetPosition)
 	{
