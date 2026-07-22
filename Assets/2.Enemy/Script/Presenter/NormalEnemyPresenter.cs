ba59f10c289dcb84ca3c000bd6f087bf25b1e38b
hanabi2356home <hanabira2356@gmail.com>
2026-07-23 02:35:43 +0900

    NormalEnemyPresenter 줄 변경

--- Assets/2.Enemy/Script/Presenter/NormalEnemyPresenter.cs
+++ Assets/2.Enemy/Script/Presenter/NormalEnemyPresenter.cs
@@ -3,36 +3,37 @@ using Zenject;
 
 public class NormalEnemyPresenter : MonoBehaviour
 {
+	[SerializeField] private string _enemyName = "Archer";
+
 	private INormalEnemyStatModel _normalEnemyStatModel;
 	private INormalEnemyView _view;
 	private IFSMMachine _fsm;
+
 	[Inject]
 	public void Initialize(INormalEnemyStatModel normalEnemyModel,
 		INormalEnemyView view,
-		IFSMMachine fsm)
+		IFSMMachine fsm	)
 	{
 		_normalEnemyStatModel = normalEnemyModel;
 		_view = view;
 		_fsm = fsm;
 
-		if(_fsm is NormalEnemyFSMMachine normalEnemyFSM)
+		
+
+		if (_fsm is NormalEnemyFSMMachine normalEnemyFSM)
 		{
 			normalEnemyFSM.BootUp();
 		}
+
 		SubscribeEvent();
 	}
-    void Start()
-    {
-        
-    }
 
-    
-    void Update()
-    {
+	void Update()
+	{
 		if (PlayerTransformProvider.PlayerTransform == null) return;
 
 		_view.UpdateTargetPosition(PlayerTransformProvider.PlayerTransform.position);
-    }
+	}
 
 	private void SubscribeEvent()
 	{
