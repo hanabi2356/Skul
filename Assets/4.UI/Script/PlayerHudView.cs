1a97aee89588ac1f2f4b0eb0ae34ecc6718e6fac
hanabi2356 <hanabira2356@gmail.com>
2026-09-10 10:22:44 +0900

    PlayerHudView 현재 보유한 골드를 UI에 업데이트 하는 함수 추가

--- Assets/4.UI/Script/PlayerHudView.cs
+++ Assets/4.UI/Script/PlayerHudView.cs
@@ -5,12 +5,22 @@ public class PlayerHudView : MonoBehaviour, IPlayerHudView
 {
 	[SerializeField] private UIDocument _uiDocument;
 	private ProgressBar _hpBar;
+	private UnityEngine.UIElements.Label _goldLabel;
+
 	public void Initialize()
 	{
 		if (_uiDocument == null || _uiDocument.rootVisualElement == null) return;
 
 		_hpBar = _uiDocument.rootVisualElement.Q<ProgressBar>("PlayerHPBar");
+		_goldLabel = _uiDocument.rootVisualElement.Q<UnityEngine.UIElements.Label>("GoldValue");
+	}
 
+	public void SetGold(int currentGold)
+	{
+		if (_goldLabel == null) return;
+
+		currentGold = Mathf.Max(0, currentGold);
+		_goldLabel.text = currentGold.ToString();
 	}
 
 	public void SetHP(int currentHP, int maxHP)
@@ -24,5 +34,6 @@ public class PlayerHudView : MonoBehaviour, IPlayerHudView
 		_hpBar.value = (float)currentHP / maxHP * 100.0f;
 	}
 
+	
 
 }
