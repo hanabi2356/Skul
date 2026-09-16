3cc4c3d92893c3e0a57f9494886d95e8a1122866
hanabi2356home <hanabira2356@gmail.com>
2026-09-17 02:18:09 +0900

    Item SO 파일 위치 이동

--- Assets/6.Item/Data/ItemData.cs
+++ Assets/6.Item/Data/ItemData.cs
@@ -12,6 +12,7 @@ public enum ItemRarity
 public class ItemData : ScriptableObject
 {
 	[SerializeField] private Sprite _itemSprite;
+	[SerializeField] private GameObject _itemPrefab;
 	[SerializeField] private string _itemID;
 	[SerializeField] private string _itemName;
 	[SerializeField] private ItemRarity _itemRarity;
@@ -20,6 +21,7 @@ public class ItemData : ScriptableObject
 
 
 	public Sprite ItemSprite => _itemSprite;
+	public GameObject ItemPrefab => _itemPrefab;
 	public string ItemID => _itemID;
 	public string ItemName => _itemName;
 	public ItemRarity ItemRarity => _itemRarity;
